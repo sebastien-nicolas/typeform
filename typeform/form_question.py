b45f19b604ab480bf71d6af741f2bc576b0c176c
Sébastien Nicolas <sebastien.nicolas@polyconseil.fr>
2017-05-12 23:15:57 +0200

    Add 'group' field to 'FormQuestion' class

--- typeform/form_question.py
+++ typeform/form_question.py
@@ -1,12 +1,13 @@
 class FormQuestion(object):
     """TypeForm form question object"""
-    __slots__ = ['_field_id', '_id', '_question']
+    __slots__ = ['_field_id', '_id', '_question', '_group']
 
-    def __init__(self, field_id=None, id=None, question=None):
+    def __init__(self, field_id=None, id=None, question=None, group=None):
         """Constructor for TypeForm form question"""
         self._field_id = field_id
         self._id = id
         self._question = question
+        self._group = group
 
     @property
     def field_id(self):
@@ -23,8 +24,13 @@ class FormQuestion(object):
         """The question asked by this question"""
         return self._question
 
+    @property
+    def group(self):
+        """The group of the question"""
+        return self._group
+
     def __repr__(self):
         return (
             'FormQuestion(field_id={field_id!r}, id={id!r}, question={question!r})'
-            .format(field_id=self.field_id, id=self.id, question=self.question)
+            .format(field_id=self.field_id, id=self.id, question=self.question, group=self.group)
         )
